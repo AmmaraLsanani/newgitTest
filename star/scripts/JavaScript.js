1a8d6cec1986d427de5f8f9370f9da1a85776f83
AmmaraLsanani <s201506058@aou.edu.sa>
2020-11-22 20:39:56 +0300

    first

---
+++ star/scripts/JavaScript.js
@@ -0,0 +1,35 @@
+﻿
+
+var checklist = document.getElementById("Checklist");
+
+var items = checklist.querySelectorAll("li");
+var input = checklist.querySelectorAll("input");
+
+
+
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", editItem);
+    input[i].addEventListener("blur", updateInput);
+    input[i].addEventListener("keypress", keyoressEnter);
+
+}
+
+function editItem() {
+    this.className = "edit";
+    var input = this.querySelector('input');
+    console.log(input.value);
+    input.focus();
+    input.setSelectionRange(0, input.value.length);
+}
+
+function updateInput() {
+    this.previousElementSibling.innerHTML = this.value;
+    this.parentNode.className = "";
+}
+function keyoressEnter(event) {
+    if (event.which === 13) {
+        updateInput.call(this);
+    }
+}
+
+
